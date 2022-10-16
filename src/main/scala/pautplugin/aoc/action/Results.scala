4fdf770f7c69f40796acf151139065f8b9b0c972
Dag Hemberg <dag.hemberg@gmail.com>
2022-10-16 18:24:18 +0200

    fat refactor

--- src/main/scala/pautplugin/aoc/action/Results.scala
+++ src/main/scala/pautplugin/aoc/action/Results.scala
@@ -3,24 +3,46 @@ package pautplugin.aoc.action
 import paut.aoc.Result
 import pautplugin.aoc._
 import pautplugin.utils.Logging
+
+// import cats.syntax.all._
+
 import java.time.LocalDate
+
 import util.chaining._
+import scala.util.Try
+import scala.util.Failure
+import scala.util.Success
 
 object Results {
   val alreadySubmittedMsg = "This solution has already been submitted and been verified to be correct."
+  val parsingErrorMsg = "Error in parsing results, please check the results file with `aoc files openDataFolder`"
+  val noResultsMsg = "No results found."
 
-  private def all = Files.read(Files.resultsFile).map(_.split("\n").toList).getOrElse(Nil)
+  private def allResults = Files
+    .read(Files.resultsFile)
+    .toRight(left = noResultsMsg)
+    .map(_.split("\n").toList)
+    .filterOrElse(_.head != "", noResultsMsg)
+    .flatMap { rawResults => 
+      Try(rawResults.map(Result.parse)) match {
+        case Success(results) => Right(results)
+        case Failure(_)       => Left(parsingErrorMsg)
+      }
+    }
 
   case object GetAll extends Action {
-    def execute = all map Result.parse foreach println
+    def execute = Logging.fromEither(allResults) { results =>
+      Logging.info(s"Found ${results.length} results.\n")
+      results.mkString("\n------------------\n").foreach(println) 
+    }
   }
 
   case class GetOne(part: Int, date: LocalDate) extends Action with Date {
-    def execute = 
-      Logging.fromOption(get, s"No result for part $part on year $year, day $day")(println)
+    def execute = Logging.fromEither(getResult)(println)
     
-    def get: Option[Result] = {
-      all find { _ startsWith s"$year;$day;$part" } map (x => Result.parse(x.trim))
+    def getResult: Either[String, Result] = allResults.flatMap { _
+      .find(res => res.day == day && res.year == year && res.part == part)
+      .toRight(left = s"No result for part $part on year $year, day $day")
     }
   }
 }
