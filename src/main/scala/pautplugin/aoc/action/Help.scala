124ef152dea6910e7f4f2581d951ca4335596501
Dag Hemberg <dag.hemberg@gmail.com>
2022-11-13 21:52:45 +0100

    added some missing info

--- src/main/scala/pautplugin/aoc/action/Help.scala
+++ src/main/scala/pautplugin/aoc/action/Help.scala
@@ -3,14 +3,15 @@ package pautplugin.aoc.action
 case class Help(d: Doc) extends Action {
   def execute = println(d.doc)
   val doc = 
-    """|
+    """|Performs various actions related to Advent of Code.
        |
        |# USAGE
        |
        |'aoc help'                                                  Prints this message
        |  'aoc help <action>'                                       Prints the documentation for a specific action
        |
-       |'aoc data <action>'
+       |'aoc data <action>'                                         Manages input data for problems
+       |  'aoc data openFolder'                                     Opens the ~/.paut config folder
        |  'aoc data initProblem "<name>" <today | <day> [year]>'    Initializes a problem
        |  'aoc data openExample <number> <today | <day> [year]>'    Opens the example file for a problem
        |  'aoc data addExample <today | <day> [year]>'              Adds an example file for a problem
