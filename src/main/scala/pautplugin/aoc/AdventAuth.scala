9b04a80d02a88ac29de537ebc0c5377b5e54dd97
Dag Hemberg <dag.hemberg@gmail.com>
2022-12-01 11:06:36 +0100

    better useragent

--- src/main/scala/pautplugin/aoc/AdventAuth.scala
+++ src/main/scala/pautplugin/aoc/AdventAuth.scala
@@ -5,7 +5,7 @@ import pautplugin.utils.Authentication
 trait AdventAuth extends Authentication {
   def tokenValue = Files.token.toRight(left = tokenMissingMsg)
   val tokenName = "session"
-  val userAgent = "paut-aoc v0.1"
+  val userAgent = "https://github.com/daghemberg/pAut.g8 by dag.hemberg@gmail.com"
   val tokenMissingMsg = 
     "No authentication token found. Please run 'aoc auth set <token>' first."
 }
